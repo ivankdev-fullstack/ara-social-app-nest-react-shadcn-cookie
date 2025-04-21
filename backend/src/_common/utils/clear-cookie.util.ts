d1404ed3ed8eeeb879ef12bd62af6cf2976adeac
Ivan Kononenko <ivan.kononenko.dev@gmail.com>
2025-04-21 08:59:32 +0300

    backend fix: cookies fixes

--- backend/src/_common/utils/clear-cookie.util.ts
+++ backend/src/_common/utils/clear-cookie.util.ts
@@ -1,11 +1,10 @@
-import { isProduction } from '@common/config/env';
 import { Response } from 'express';
 
 export const clearAuthCookie = (res: Response) => {
   res.clearCookie('token', {
     httpOnly: true,
-    secure: isProduction,
-    sameSite: isProduction ? 'none' : 'lax',
+    secure: true,
+    sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7d
   });
 };
--- backend/src/_common/utils/set-cookie.util.ts
+++ backend/src/_common/utils/set-cookie.util.ts
@@ -1,11 +1,10 @@
-import { isProduction } from '@common/config/env';
 import { Response } from 'express';
 
 export const setAuthCookie = (res: Response, token: string) => {
   res.cookie('token', token, {
     httpOnly: true,
-    secure: isProduction,
-    sameSite: isProduction ? 'none' : 'lax',
+    secure: true,
+    sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7d
   });
 };
